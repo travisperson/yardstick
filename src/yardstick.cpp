172c55308ecc883cd8e10359da36d3ca66616184
Silver <jeromyj@gmail.com>
2012-10-03 20:48:14 -0700

    changed one billion to a constant, numbers look messy

--- src/yardstick.cpp
+++ src/yardstick.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <iomanip>
 
+#define BILLION 1000000000
+
 double toSeconds(struct timespec ts)
 {
-	return ts.tv_sec + (ts.tv_nsec / 1000000000);
+	return ts.tv_sec + (ts.tv_nsec / BILLION);
 }
 
 struct timespec
@@ -17,7 +19,7 @@ timespec_diff(struct timespec start, struct timespec end)
     // If the nano seconds go below zero we will want to shift 1 second to
     // the nano seconds.
     end.tv_sec  -= 1;
-    end.tv_nsec += 1000000000; 
+    end.tv_nsec += BILLION; 
   }
 
   diff.tv_sec  = end.tv_sec - start.tv_sec;
@@ -36,12 +38,12 @@ timespec_avg ( const std::list<struct timespec> &time_trials )
   
   for ( ; itr != time_trials.end(); itr++ )
   {
-    nano_sum += (*itr).tv_nsec + ((*itr).tv_sec * 1000000000);
+    nano_sum += (*itr).tv_nsec + ((*itr).tv_sec * BILLION);
   }
   
   nano_sum = (time_t) nano_sum / (time_t) time_trials.size(); 
-  ts_avg.tv_sec  = (time_t) nano_sum / (time_t) 1000000000;
-  ts_avg.tv_nsec = (time_t) nano_sum % (time_t) 1000000000;
+  ts_avg.tv_sec  = (time_t) nano_sum / (time_t) BILLION;
+  ts_avg.tv_nsec = (time_t) nano_sum % (time_t) BILLION;
  
   return ts_avg;
 }
@@ -51,11 +53,11 @@ timespec_round_avg ( struct timespec ts_avg, int runs )
 {
   time_t nano_sum;
   
-  nano_sum = ts_avg.tv_nsec + ts_avg.tv_sec * 1000000000;
+  nano_sum = ts_avg.tv_nsec + ts_avg.tv_sec * BILLION;
 
   nano_sum = (time_t) nano_sum / (time_t) runs; 
-  ts_avg.tv_sec  = (time_t) nano_sum / (time_t) 1000000000;
-  ts_avg.tv_nsec = (time_t) nano_sum % (time_t) 1000000000;
+  ts_avg.tv_sec  = (time_t) nano_sum / (time_t) BILLION;
+  ts_avg.tv_nsec = (time_t) nano_sum % (time_t) BILLION;
  
   return ts_avg;
 }
